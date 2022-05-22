2f3eb58fe1cce75b4bc5cb501fe2d46735db5fc3
zomi n <jly14@tsinghua.org.cn>
2022-05-22 23:09:47 +0200

    complete autoencoder

--- Assignment 3/autoencoder_template.py
+++ Assignment 3/autoencoder_template.py
@@ -7,10 +7,33 @@ class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
         # create layers here
+        self.conv_first = nn.Sequential(
+            nn.Conv2d(1,16,3,padding=1,bias=True),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.conv = nn.Sequential(
+            nn.Conv2d(16,16,3,padding=1,bias=True),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.conv_last = nn.Sequential(
+            nn.Conv2d(16,1,2,padding=(2,1),bias=True),
+            nn.BatchNorm2d(1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
         
     def forward(self, x):
         # use the created layers here
-        h = x
+        # x --> 32,32,1
+        x = self.conv_first(x) # 16,16,16
+        x = self.conv(x) # 8,8,16
+        x = self.conv(x) # 4,4,16
+        x = self.conv(x) # 2,2,16
+        h = self.conv_last(x) # 1,2,1
         return h
     
 # %%  Decoder
@@ -18,11 +41,33 @@ class Decoder(nn.Module):
     def __init__(self):
         super(Decoder, self).__init__()
         # create layers here
+        self.conv_first = nn.Sequential(
+            nn.ConvTranspose2d(1,16,3,padding=1,bias=True),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=(1,2),mode='nearest')
+        )
+        self.conv = nn.Sequential(
+            nn.ConvTranspose2d(16,16,3,padding=1,bias=True),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2,mode='nearest')
+        )
+        self.conv_last = nn.Sequential(
+            nn.ConvTranspose2d(16,1,3,padding=1,bias=True),
+            nn.BatchNorm2d(1),
+            nn.Upsample(scale_factor=2,mode='nearest')
+        )
         
-    def forward(self, h):
+    def forward(self, x):
         # use the created layers here
-        r = h
-        return r
+        # x --> 1,2,1
+        x = self.conv_first(x) # 2,2,16
+        x = self.conv(x) # 4,4,16
+        x = self.conv(x) # 8,8,16
+        x = self.conv(x) # 16,16,16
+        h = self.conv_last(x) # 32,32,1
+        return h
     
 # %%  Autoencoder
 class AE(nn.Module):
@@ -34,5 +79,25 @@ class AE(nn.Module):
     def forward(self, x):
         h = self.encoder(x)
         r = self.decoder(h)
-        return r, h
-    
+        return h,r
+        
+#%%
+def sanity_check():
+
+    x = torch.randn((64,1,32,32))
+
+    model_1, model_2, model = Encoder(), Decoder(), AE()
+
+    latent = model_1(x)
+    out = model_2(latent)
+    y1, y2 = model(x)
+    print('Encoder',latent.shape) # should be 64,16,2,1
+    print('Decoder',out.shape) # should be 64,1,32,32
+    print('AE_latent',y1.shape) # should be 64,16,2,1
+    print('AE_output',y2.shape) # should be 64,1,32,32
+
+# %%
+if __name__ == "__main__":
+    sanity_check()
+
+# %%
--- Assignment 3/main_template.py
+++ Assignment 3/main_template.py
@@ -3,6 +3,8 @@
 import torch
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import torch.nn as nn
+import numpy as np
 
 # local imports
 import MNIST_dataloader
@@ -13,20 +15,21 @@ torch.random.manual_seed(0)
 
 # %% preperations
 # define parameters
-data_loc = 'D://5LSL0-Datasets' #change the data location to something that works for you
+data_loc = '5LSL0-Datasets' #change the data location to something that works for you
 batch_size = 64
-no_epochs = 4
-learning_rate = 3e-4
+no_epochs = 30
+learning_rate = 1e-4
 
 # get dataloader
 train_loader, test_loader = MNIST_dataloader.create_dataloaders(data_loc, batch_size)
 
 # create the autoencoder
-AE = autoencoder_template.AE()
+model = autoencoder_template.AE()
 
 # create the optimizer
-
-
+optimizer = torch.optim.Adam(model.parameters(),lr = learning_rate)
+criterion = nn.MSELoss()
+train_loss = []
 # %% training loop
 # go over all epochs
 for epoch in range(no_epochs):
@@ -34,20 +37,59 @@ for epoch in range(no_epochs):
     # go over all minibatches
     for batch_idx,(x_clean, x_noisy, label) in enumerate(tqdm(train_loader)):
         # fill in how to train your network using only the clean images
-        1+1
+        if torch.cuda.is_available():
+            device = torch.device('cuda:0')
+            x_clean, x_noisy, label = [x.cuda() for x in [x_clean, x_noisy, label]]
+            model.to(device)
+        latent, score = model(x_clean)
+        loss = criterion(score,x_clean)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        train_loss.append(loss.item())
+    print('Loss',np.mean(np.array(train_loss)))
 
+#%%
+# move back to cpu    
+# get some examples
+examples = enumerate(test_loader)
+_, (x_clean_example, x_noisy_example, labels_example) = next(examples)
+latent, score = model(x_clean_example.cuda())
+latent = latent.data.cpu().numpy()
+score = score.data.cpu().numpy()
+# show the examples in a plot
+plt.figure(figsize=(12,3))
+for i in range(10):
+    plt.subplot(3,10,i+1)
+    plt.imshow(x_clean_example[i,0,:,:],cmap='gray')
+    plt.xticks([])
+    plt.yticks([])
+    
+    plt.subplot(3,10,i+11)
+    plt.imshow(latent[i,0,:,:],cmap='gray')
+    plt.xticks([])
+    plt.yticks([])
+    
+    plt.subplot(3,10,i+21)
+    plt.imshow(score[i,0,:,:],cmap='gray')
+    plt.xticks([])
+    plt.yticks([])
+
+plt.tight_layout()
+#plt.savefig("exercise_1.png",dpi=300,bbox_inches='tight')
+plt.show() 
 
 # %% HINT
-#hint: if you do not care about going over the data in mini-batches but rather want the entire dataset use:
-x_clean_train = train_loader.dataset.Clean_Images
-x_noisy_train = train_loader.dataset.Noisy_Images
-labels_train  = train_loader.dataset.Labels
+# #hint: if you do not care about going over the data in mini-batches but rather want the entire dataset use:
+# x_clean_train = train_loader.dataset.Clean_Images
+# x_noisy_train = train_loader.dataset.Noisy_Images
+# labels_train  = train_loader.dataset.Labels
 
-x_clean_test  = test_loader.dataset.Clean_Images
-x_noisy_test  = test_loader.dataset.Noisy_Images
-labels_test   = test_loader.dataset.Labels
+# x_clean_test  = test_loader.dataset.Clean_Images
+# x_noisy_test  = test_loader.dataset.Noisy_Images
+# labels_test   = test_loader.dataset.Labels
 
-# use these 10 examples as representations for all digits
-x_clean_example = x_clean_test[0:10,:,:,:]
-x_noisy_example = x_noisy_test[0:10,:,:,:]
-labels_example = labels_test[0:10]
+# # use these 10 examples as representations for all digits
+# x_clean_example = x_clean_test[0:10,:,:,:]
+# x_noisy_example = x_noisy_test[0:10,:,:,:]
+# labels_example = labels_test[0:10]
